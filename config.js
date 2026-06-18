4a8cd6403f821985d097d41fb991cab08cbc94ae
dombejar <dominicbejar@gmail.com>
2026-06-18 16:13:33 -0700

    Update Supabase anon key in config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Find them at: supabase.com → your project → Settings → API
 
 const SUPABASE_URL = 'https://llkmdkboxbxsfnknhriz.supabase.co/rest/v1/'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxsa21ka2JveGJ4c2Zua25ocml6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4MjI4NzMsImV4cCI6MjA5NzM5ODg3M30.f8SP1V7YnXk1kiLv0oC5NS0Xs1uxnA6PZ2jdmGZNX44'
+const SUPABASE_ANON_KEY = 'sb_publishable_CkK1ZnoiimC52OGZSc5guA_YCKpch99'
